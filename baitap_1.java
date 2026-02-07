9b7c6bce21967f6cb4b617a71b768a8010e8a8ab
Lê Thanh Hướng <lethanhhuong484@gmail.com>
2026-02-07 21:15:16 +0700

    Add files via upload

---
+++ baitap_1.java
@@ -0,0 +1,21 @@
+package baitap1;
+
+public class baitap_1 {
+
+	public static void main(String[] args) {
+		
+			int a=15;
+			int b=4;
+			double c=3.5;
+			
+			int tong = a + b;
+			double thuong = a / c;
+			
+		
+		System.out.println("Tổng hai số a và b: "+ tong);
+		System.out.println("Thương hai số a và c: "+ thuong);
+		
+
+	}
+
+}
