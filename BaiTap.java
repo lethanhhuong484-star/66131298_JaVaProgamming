02ab9843766ea6c529fd1c896c2cd3d9e3e98212
Lê Thanh Hướng <lethanhhuong484@gmail.com>
2026-02-07 21:15:58 +0700

    Add files via upload

---
+++ BaiTap.java
@@ -0,0 +1,13 @@
+package baitap2;
+
+public class BaiTap {
+
+	public static void main(String[] args) {
+		double C=25.0;
+		double F=(C*9/5)+32;
+		
+		System.out.println( " độ F :" + F );
+
+	}
+
+}
