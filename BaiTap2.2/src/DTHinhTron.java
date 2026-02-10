7fa16bf788798068eabb1f7b328a77f8846f472b
Lê Thanh Hướng <lethanhhuong484@gmail.com>
2026-02-10 19:41:27 +0700

    bai2.2

---
+++ BaiTap2.2/src/DTHinhTron.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class DTHinhTron {
+
+	public static void main(String[] args) {
+	      Scanner DT=new Scanner(System.in);
+	       System.out.print("nhập bán kính r:");
+	       double r=DT.nextDouble();
+	       
+	       	double Dt=r*r*3.14;
+	       	
+	       	System.out.print("diện tích hình tròn:"+Dt);
+
+	}
+
+}
