17c7bf4ce22ee1dfa240dff19622d1718414b266
Lê Thanh Hướng <lethanhhuong484@gmail.com>
2026-02-17 11:29:14 +0700

    bài vận tốc

---
+++ BaiTap3.3/src/V_tudo.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class V_tudo {
+
+	public static void main(String[] args) {
+		Scanner v= new Scanner(System.in);
+		
+		System.out.print("độ cao h:");
+        double h=v.nextDouble();		
+      double  V=Math.sqrt(2*9.8*h);
+        System.out.println("vận tốc rơi tự do của vật:" +V );
+        
+        
+        
+	}
+
+	
+	}
+
+
