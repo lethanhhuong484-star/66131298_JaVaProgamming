6fae852f23c4c3d98598ad37b20e7435a62cccd3
Lê Thanh Hướng <lethanhhuong484@gmail.com>
2026-03-05 07:40:02 +0700

    bai lop

---
+++ bailop/src/bailop/CTChinh.java
@@ -0,0 +1,14 @@
+package bailop;
+
+public class CTChinh {
+
+	public static void main(String[] args) {
+	SinhVien sinhvien1= new SinhVien();
+	sinhvien1.HovaTen="Lê Thanh Hướng";
+	sinhvien1.tuoi="20";
+	sinhvien1.MSSV="66131298";
+	
+	sinhvien1.LamBai("đã làm");
+	}
+
+}
---
+++ bailop/src/bailop/SinhVien.java
@@ -0,0 +1,12 @@
+package bailop;
+
+public class SinhVien {
+ String HovaTen;
+ String tuoi;
+ String MSSV;
+ 
+ void LamBai(String SoBai) {
+	 System.out.println(HovaTen +" đã làm bài tập: " + SoBai);
+	 
+ }
+}
