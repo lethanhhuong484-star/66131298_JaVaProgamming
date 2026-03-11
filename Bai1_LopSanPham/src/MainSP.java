e70fb6e7ed489cd3742e64b45572438b0bb4bd13
Lê Thanh Hướng <lethanhhuong484@gmail.com>
2026-03-11 10:16:29 +0700

    lopsanpham

---
+++ Bai1_LopSanPham/src/MainSP.java
@@ -0,0 +1,9 @@
+
+public class MainSP {
+
+	public static void main(String[] args) {
+		        SanPham sp = new SanPham("SP01", "Bút", 5000, 10);
+		        sp.hienThiThongTin();
+		    }
+
+	}
---
+++ Bai1_LopSanPham/src/SanPham.java
@@ -0,0 +1,64 @@
+class SanPham {
+    private String maSP;
+    private String tenSP;
+    private double gia;
+    private int soLuong;
+
+    public SanPham() {
+    }
+    public SanPham(String maSP, String tenSP, double gia, int soLuong) {
+        setMaSP(maSP);
+        setTenSP(tenSP);
+        setGia(gia);
+        setSoLuong(soLuong);
+    }
+    public String getMaSP() {
+        return maSP;
+    }
+
+    public void setMaSP(String maSP) {
+        if (maSP != null && !maSP.isEmpty()) {
+            this.maSP = maSP;
+        } else {
+            System.out.println("Mã sản phẩm không được rỗng");
+        }
+    }
+    public String getTenSP() {
+        return tenSP;
+    }
+    public void setTenSP(String tenSP) {
+        this.tenSP = tenSP;
+    }
+    public double getGia() {
+        return gia;
+    }
+    public void setGia(double gia) {
+        if (gia > 0) {
+            this.gia = gia;
+        } else {
+            System.out.println("Giá phải > 0");
+        }
+    }
+    public int getSoLuong() {
+        return soLuong;
+    }
+    public void setSoLuong(int soLuong) {
+        if (soLuong >= 0) {
+            this.soLuong = soLuong;
+        } else {
+            System.out.println("Số lượng phải >= 0");
+        }
+    }
+
+    public double tinhThanhTien() {
+        return gia * soLuong;
+    }
+
+    public void hienThiThongTin() {
+        System.out.println("Mã SP: " + maSP);
+        System.out.println("Tên SP: " + tenSP);
+        System.out.println("Giá: " + gia);
+        System.out.println("Số lượng: " + soLuong);
+        System.out.println("Thành tiền: " + tinhThanhTien());
+    }
+}
