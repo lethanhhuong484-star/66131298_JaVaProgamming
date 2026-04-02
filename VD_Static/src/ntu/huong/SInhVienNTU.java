46d02ea8f2d9281c0cf5c7257f67e73921530f0c
Lê Thanh Hướng <lethanhhuong484@gmail.com>
2026-04-02 21:25:35 +0700

    ví dụ sử dụng static(mảng tĩnh)

---
+++ VD_Static/src/ntu/huong/SInhVienNTU.java
@@ -0,0 +1,11 @@
+package ntu.huong;
+
+public class SInhVienNTU {
+	public static String tenTruong="trường đại học nha trang";
+	 public static void sayHi() {
+		 System.out.print("xin chào NTU!");
+	 }
+		public void sayHello() {
+			System.out.print("xin chào bạn");
+		}
+}
---
+++ VD_Static/src/ntu/huong/run1.java
@@ -0,0 +1,16 @@
+package ntu.huong;
+
+public class run1 {
+
+	public static void main(String[] args) {
+		System.out.print(Math.PI);
+		System.out.print(Math.sqrt(9));
+
+		System.out.println(SInhVienNTU.tenTruong);
+		SInhVienNTU.sayHi();
+
+		SInhVienNTU sv1=new SInhVienNTU();
+		sv1.sayHello();
+	}
+
+}
