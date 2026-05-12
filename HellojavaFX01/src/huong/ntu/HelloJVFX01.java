695778a6d45210198ded75f4e6fa65319ddc2ce4
Lê Thanh Hướng <lethanhhuong484@gmail.com>
2026-05-12 08:28:22 +0700

    hellojavafx01 hoàn chỉnh

--- HellojavaFX01/src/huong/ntu/HelloJVFX01.java
+++ HellojavaFX01/src/huong/ntu/HelloJVFX01.java
@@ -1,22 +1,36 @@
 package huong.ntu;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class HelloJVFX01 extends Application {
 
     @Override
-    public void start(Stage stage) {
+    public void start(Stage primaryStage) {
+        
+        Button btn = new Button();
+        btn.setText("Say 'Hello World'");
+       
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("Hello World!");
+            }
+        });
 
-        Label label = new Label("Xin chào JavaFX");
+        StackPane root = new StackPane();
+        root.getChildren().add(btn);
 
-        Scene scene = new Scene(label, 400, 200);
+        Scene scene = new Scene(root, 300, 250);
 
-        stage.setTitle("JavaFX");
-        stage.setScene(scene);
-        stage.show();
+        primaryStage.setTitle("Hello World!");
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 
     public static void main(String[] args) {
